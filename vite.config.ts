cf7fbcdfb467182755c56289d017f3fcff227b76
jengmun <wongjm4@hotmail.com>
2025-01-12 15:36:40 +0800

    config: update vite config

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import { defineConfig } from "vite";
 export default defineConfig({
   plugins: [react()],
   resolve: {
+    extensions: [".tsx", ".ts", ".jsx", ".js"],
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
